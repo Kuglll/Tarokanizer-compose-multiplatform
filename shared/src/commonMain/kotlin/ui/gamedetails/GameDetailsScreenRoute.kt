8399502663528224fca2d0d0f2c62c1279ce91fd
Kuglll <mark.frelih@gmail.com>
2024-01-18 16:13:29 +0100

    Implement GameDetailsScreen UI

--- shared/src/commonMain/kotlin/ui/gamedetails/GameDetailsScreenRoute.kt
+++ shared/src/commonMain/kotlin/ui/gamedetails/GameDetailsScreenRoute.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Divider
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
@@ -28,9 +28,13 @@ import androidx.compose.material.icons.rounded.AddCircle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -81,10 +85,10 @@ fun GameDetailsScreen(
             )
         },
         rounds = listOf(
-            Round(listOf(0,0,0,0,0,0,0,0)),
-            Round(listOf(0,0,0,0,0,0,0,0)),
-            Round(listOf(0,0,0,0,0,0,0,0)),
-            Round(listOf(0,0,0,0,0,0,0,0)),
+            Round(listOf(0, 0, 0, 0, 0, 0, 0, 0)),
+            Round(listOf(0, 2, 0, 0, 10, 0, 0, 0)),
+            Round(listOf(0, 0, 0, 0, 70, 0, 0, 0)),
+            Round(listOf(0, 0, 350, 0, 0, 0, 0, 0)),
         ),
     )
 }
@@ -123,43 +127,15 @@ fun GameDetailsScreenContent(
         val scroll = rememberScrollState()
 
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize().horizontalScroll(scroll),
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth().horizontalScroll(scroll),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-            ) {
-                players.forEachIndexed { index, player ->
-                    Row {
-                        Column {
-                            Text(
-                                text = player.name,
-                                modifier = Modifier.width(80.dp).padding(4.dp),
-                                maxLines = 1,
-                                overflow = TextOverflow.Ellipsis,
-                                textAlign = TextAlign.Center,
-                            )
-                            Divider(
-                                modifier = Modifier
-                                    .width(80.dp)
-                                    .height(2.dp),
-                                color = Color.Black,
-                            )
-                        }
-                        if(index != players.size - 1){
-                            Divider(
-                                modifier = Modifier
-                                    .width(2.dp)
-                                    .fillMaxHeight(),
-                                color = Color.Black,
-                            )
-                        }
-                    }
-                }
-            }
-            //TODO: Add UI for rounds
+            PlayerNamesRow(
+                players = players,
+            )
+            RoundsContent(
+                rounds = rounds,
+            )
         }
-        //TODO: Check how horizontal scroll works together with vertical scroll
     }
 
 }
@@ -190,5 +166,103 @@ private fun GameDetailsScreenTopBar(
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold
         )
+    }
+}
+
+@Composable
+private fun PlayerNamesRow(
+    players: List<Player>
+) {
+    val playerRowHeight = remember { mutableStateOf(0.dp) }
+
+    val density = LocalDensity.current
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        players.forEachIndexed { index, player ->
+            Row(
+                modifier = Modifier.onGloballyPositioned {
+                    with(density){
+                        playerRowHeight.value = it.size.height.toDp()
+                    }
+                }
+            ) {
+                Column {
+                    Text(
+                        text = player.name,
+                        modifier = Modifier.width(80.dp).padding(4.dp),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        textAlign = TextAlign.Center,
+                    )
+                    Divider(
+                        modifier = Modifier
+                            .width(80.dp)
+                            .height(2.dp),
+                        color = Color.Black,
+                    )
+                }
+                if (index != players.size - 1) {
+                    Divider(
+                        modifier = Modifier
+                            .width(2.dp)
+                            .height(playerRowHeight.value),
+                        color = Color.Black,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun RoundsContent(
+    rounds: List<Round>,
+) {
+    val singleRoundRowHeight = remember { mutableStateOf(0.dp) }
+
+    val density = LocalDensity.current
+
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
+        rounds.forEach { round ->
+            Row(
+                modifier = Modifier.onGloballyPositioned {
+                    with(density) {
+                        singleRoundRowHeight.value = it.size.height.toDp()
+                    }
+                }
+            ) {
+                round.points.forEachIndexed { index, it ->
+                    Row {
+                        Column {
+                            Text(
+                                text = it.toString(),
+                                modifier = Modifier.width(80.dp),
+                                textAlign = TextAlign.Center,
+                                fontSize = 20.sp,
+                            )
+                            Divider(
+                                modifier = Modifier
+                                    .width(80.dp)
+                                    .height(2.dp),
+                                color = Color.Black,
+                            )
+                        }
+                        if (index != round.points.size - 1) {
+                            Divider(
+                                modifier = Modifier
+                                    .width(2.dp)
+                                    .height(singleRoundRowHeight.value),
+                                color = Color.Black,
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 }
